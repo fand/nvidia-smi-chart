bdb74d43487c86fe6eb46f12c88ca8f39ce8f43e
fand <mail@amagi.dev>
2022-04-20 14:28:08 +0900

    init

---
+++ index.js
@@ -0,0 +1,45 @@
+const asciichart = require("asciichart");
+const cp = require("child_process");
+
+const s0 = [];
+
+function summarize(input, length) {
+  const scale = Math.ceil(input.length / length);
+
+  const retCount = Math.floor(input.length / scale);
+  const ret = [];
+  for (let i = 0; i < retCount; i++) {
+    ret[i] = 0;
+    for (let j = 0; j < scale; j++) {
+      ret[i] += input[i * scale + j];
+    }
+    ret[i] /= scale;
+  }
+
+  return ret;
+}
+
+const getUsedMemory = () => {
+  return parseFloat(
+    cp
+      .execSync("nvidia-smi --query-gpu=memory.used --format=csv,nounits")
+      .toString()
+      .split("\n")[1]
+  );
+};
+
+const memTotal = parseFloat(
+  cp
+    .execSync("nvidia-smi --query-gpu=memory.total --format=csv,nounits")
+    .toString()
+    .split("\n")[1]
+);
+
+setInterval(() => {
+  s0.push(getUsedMemory());
+  const q0 = summarize(s0, 100);
+
+  console.clear();
+  console.log(asciichart.plot(q0, { height: 15 }));
+  console.log("Total memory:", memTotal, "(MiB)");
+}, 1000);
